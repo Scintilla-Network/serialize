94d0205d5311927e0b2a79565228c9e538cd2949
Alex Werner <obusco@gmail.com>
2025-10-25 23:37:11 +0200

    feat: export llog.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { serialize } from './serialize/index.js';
 import { deserialize } from './deserialize/index.js';
 import { NET_KINDS, NET_KINDS_ARRAY } from './NET_KINDS.js';
+import { llog } from './llog.js';
 
-export { serialize, deserialize, NET_KINDS, NET_KINDS_ARRAY };
+export { serialize, deserialize, NET_KINDS, NET_KINDS_ARRAY, llog };
